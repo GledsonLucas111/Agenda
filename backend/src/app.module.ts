e389be8b3ada7965b334ac37b724470bc9336da1
gledsonlucas111 <gledsonlucas111@gmail.com>
2025-02-28 16:07:33 -0300

    update folders

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './app/users/users.module';
-import { ScheduleModule } from './app/schedule/schedule.module';
+import { UsersModule } from './users/users.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
   imports: [UsersModule, ScheduleModule],
